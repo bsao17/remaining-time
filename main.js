09043c3a0cb1ff1231d4609ab453b5ca7532035a
Bruno M <declic62gmail.com>
2023-07-07 11:13:33 +0200

    feat: add counter and div with remainig time

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-let time_counter = document.querySelector("#time-counter")
+/**
+ * Prototype Javascript script for futur Chrome Extension
+ */
+
+let time = document.querySelector("#time")
 let article_size = document.querySelector("#article").textContent.trim()
 let article_words = article_size.split(" ")
 let time_array = []
-let space = "/\s/g"
 
 
-const time_remaining = (article)=>{
+const words_request = (article)=>{
     article_words.forEach((word)=>{
-        if(!word.replace(space, " ")){
+        if(!word.replace("/\s/g")){
             return
         }else {
             time_array.push(word)
         }
     })
-    console.log(time_array.length)
+    console.log(time_array)
 }
 
-time_remaining(article_size)
+const create_time_remaining_div = (words_array) => {
+    time_remain = document.createElement("span")
+    time_remain.innerHTML = ` Between ${time_array.length / 300 * 60} & ${time_array.length / 200 * 60} secondes`
+    time.append(time_remain)
+} 
+    
+
+words_request(article_size)
+create_time_remaining_div(time_array)
