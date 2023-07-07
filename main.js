18f7a74778c2e14b8baa9bb5118afabae06060fd
Bruno M <declic62gmail.com>
2023-07-07 11:58:00 +0200

    feat: update and lint the script

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 const time = document.querySelector("#time")
 const article_size = document.querySelector("#article").textContent.trim()
 const article_words = article_size.split(" ")
-const time_array = []
+const words_array = []
 
 
 const words_recover = (article)=>{
@@ -13,18 +13,18 @@ const words_recover = (article)=>{
         if(!word.replace("/\s/g")){
             return
         }else {
-            time_array.push(word)
+            words_array.push(word)
         }
     })
-    return time_array
+    return words_array
 }
 
 const create_time_remaining_div = (words_array) => {
     time_remain = document.createElement("span")
     time_remain.className = "text-danger"
-    time_remain.innerHTML = ` Between ${time_array.length / 300 * 60} & ${time_array.length / 200 * 60} secondes`
+    time_remain.innerHTML = ` Between ${words_array.length / 300 * 60} & ${words_array.length / 200 * 60} secondes`
     time.append(time_remain)
 } 
     
 words_recover(article_size)
-create_time_remaining_div(time_array)
+create_time_remaining_div(words_array)
